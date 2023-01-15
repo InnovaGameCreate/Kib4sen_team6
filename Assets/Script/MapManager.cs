f66741e10decc6ab232ef321800987a023da3a9b
s4r6 <kouta4466@icloud.com>
2023-01-15 20:02:31 +0900

    ブロックの置き換え
    雪玉が落ちた場所にはブロックが置かれ、右クリックを押すと画面の中心のブロックが置き替えられる

--- Assets/Script/MapManager.cs
+++ Assets/Script/MapManager.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     static public MapManager instance;
+    private bool DownFlag;
+    private bool UpFlag;
+    [SerializeField]    //D:凹む, U:盛り上がる
+    private GameObject[] D_CBlockPrefab;  //中央
     [SerializeField]
-    private GameObject[] CBlockPrefab;  //中央
+    private GameObject[] D_LBlockPrefab;  //左
     [SerializeField]
-    private GameObject[] LBlockPrefab;  //左
+    private GameObject[] D_RBlockPrefab;  //右
     [SerializeField]
-    private GameObject[] RBlockPrefab;  //右
+    private GameObject[] D_UBlockPrefab;  //上
     [SerializeField]
-    private GameObject[] UBlockPrefab;  //上
+    private GameObject[] D_DBlockPrefab;  //下
     [SerializeField]
-    private GameObject[] DBlockPrefab;  //下
+    private GameObject[] U_CBlockPrefab;  //中央
+    [SerializeField]
+    private GameObject[] U_LBlockPrefab;  //左
+    [SerializeField]
+    private GameObject[] U_RBlockPrefab;  //右
+    [SerializeField]
+    private GameObject[] U_UBlockPrefab;  //上
+    [SerializeField]
+    private GameObject[] U_DBlockPrefab;  //下
     private MapInfo[,] mapinfos;
     private Vector3 Temp_Pos;
     public struct MapInfo
     {
-        public int y;   //y軸方向の情報
+        public float y;   //y軸方向の情報
         public int SnowCount;   //雪を掘った回数
         public int SnowState;   //雪の積もり具合の状態
         //0:デフォルト, -1:1段階凹み, -2:2段階凹み, -3:3段階凹み, -4:4段階(破壊)//
-        public MapInfo(int y = 0, int SnowCount = 0, int SnowState = 0)    //コンストラクタ
+        //0:デフォルト, 1:1段階盛り, 2:2段階盛り, 3:3段階盛り(ブロック一つ)
+        public MapInfo(float y = 0, int SnowCount = 0, int SnowState = 0)    //コンストラクタ
         {
             this.y = y;
             this.SnowCount = SnowCount;
@@ -54,6 +67,8 @@ public class MapManager : MonoBehaviour
                 mapinfos[i,j] = new MapInfo(0, 0, 0);  //メンバを0で初期化
             }
         }
+        UpFlag = false;
+        DownFlag = false;
     }
 
     // Update is called once per frame
@@ -67,51 +82,88 @@ public class MapManager : MonoBehaviour
 
     }
 
-    public void ChangeBlock(GameObject block, Transform blockPos)
+    public void ChangeBlock(GameObject block, Transform blockPos, int Check)
     {
 
-        ChangeSnowState(block, blockPos);
+        ChangeSnowState(block, blockPos, Check);
 
-        if (mapinfos[(int)blockPos.position.x,(int)blockPos.position.y].SnowState == -1) //凹むようにブロックを置き換え
+        if (mapinfos[(int)blockPos.position.x,(int)blockPos.position.z].SnowState == -1) //凹むようにブロックを置き換え
         {
             Destroy(block);
-            Instantiate(CBlockPrefab[1], blockPos.position, Quaternion.identity);
+            Instantiate(D_CBlockPrefab[1], blockPos.position, Quaternion.identity);
         }
-        if (mapinfos[(int)blockPos.position.x, (int)blockPos.position.y].SnowState == -2)
+        if (mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState == -2)
         {
             Destroy(block);
-            Instantiate(CBlockPrefab[2], Temp_Pos, Quaternion.identity);
+            Instantiate(D_CBlockPrefab[2], Temp_Pos, Quaternion.identity);
         }
-        if (mapinfos[(int)blockPos.position.x, (int)blockPos.position.y].SnowState == -3)
+        if (mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState == -3)
         {
             Destroy(block);
-            blockPos.position += new Vector3(0, -0.35f, 0);
-            Instantiate(CBlockPrefab[3], Temp_Pos, Quaternion.identity);
+            Instantiate(D_CBlockPrefab[3], Temp_Pos, Quaternion.identity);
         }
-        if (mapinfos[(int)blockPos.position.x, (int)blockPos.position.y].SnowState == -4)
+        if (mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState == -4)
         {
             Destroy(block);
         }
+
+        if (mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState == 1)    //盛り上がるようにブロックを設置
+        {
+            UpFlag = false; //一度だけ実行されるように
+            Instantiate(U_CBlockPrefab[0], Temp_Pos, Quaternion.identity);
+        }
+        if (mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState == 2)
+        {
+            Destroy(block);
+            UpFlag = false;
+            Instantiate(U_CBlockPrefab[1], Temp_Pos, Quaternion.identity);
+        }
+        if (mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState == 3)
+        {
+            Destroy(block);
+            UpFlag = false;
+            Instantiate(U_CBlockPrefab[2], Temp_Pos, Quaternion.identity);
+        }
+
     }
 
-    private void ChangeSnowState(GameObject block, Transform blockPos)  //雪玉が当たるたびに状態を変更
+    private void ChangeSnowState(GameObject block, Transform blockPos, int Check)  //雪玉が当たるたびに状態を変更
     {
-        mapinfos[(int)blockPos.position.x, (int)blockPos.position.y].SnowCount--;   //雪玉が当たるたびにマイナス
-        switch(mapinfos[(int)blockPos.position.x, (int)blockPos.position.y].SnowCount)
+        if(Check == 1)
+            mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowCount++;   //雪玉が当たるたびにプラス
+        else
+            mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowCount--;   //右クリックのたびにマイナス
+        
+        switch (mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowCount)
         {
             case -1:
-                mapinfos[(int)blockPos.position.x, (int)blockPos.position.y].SnowState = -1;
+                mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState = -1;
                 break;
             case -3:
-                mapinfos[(int)blockPos.position.x, (int)blockPos.position.y].SnowState = -2;
+                mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState = -2;
                 Temp_Pos = blockPos.position + new Vector3(0, -0.2f, 0);
                 break;
             case -7:
-                mapinfos[(int)blockPos.position.x, (int)blockPos.position.y].SnowState = -3;
+                mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState = -3;
                 Temp_Pos = blockPos.position + new Vector3(0, -0.35f, 0);
                 break;
             case -10:
-                mapinfos[(int)blockPos.position.x, (int)blockPos.position.y].SnowState = -4;
+                mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState = -4;
+                break;
+            case 1:
+                mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState = 1;
+                mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].y += 1; //ブロックを置くy座標を計算
+                Temp_Pos = blockPos.position + new Vector3(0, mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].y, 0);  //ブロックを設置するy座標を設定
+                UpFlag = true;
+                break;
+            case 3:
+                mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState = 2;
+                UpFlag = true;
+                break;
+            case 7:
+                mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowState = 3;
+                UpFlag = true;
+                mapinfos[(int)blockPos.position.x, (int)blockPos.position.z].SnowCount = 0; //カウントをリセット
                 break;
         }
     }
--- Assets/Script/PlayerController_Test.cs
+++ Assets/Script/PlayerController_Test.cs
@@ -7,40 +7,31 @@ public class PlayerController_Test : MonoBehaviour
     private Rigidbody myrigidbody;
     [SerializeField]
     private GameObject ballPrefab;
+    Vector2 displayCenter;
+    private Transform pos;
+    private GameObject block;
+    const int DOWN = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        displayCenter = new Vector2(Screen.width / 2, Screen.height / 2);
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMove();
-
     }
     private void PlayerMove()
     {
-        if(Input.GetKey(KeyCode.A))
-        {
-            this.transform.Translate(-0.01f, 0.0f, 0.0f);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            this.transform.Translate(0.01f, 0.0f, 0.0f);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            this.transform.Translate(0.0f, 0.0f, 0.01f);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            this.transform.Translate(0.0f, 0.0f, -0.01f);
-        }
         if (Input.GetMouseButton(0))
         {
             ShotSnowBall();
+        }
+        if (Input.GetMouseButton(1))
+        {
+            ShotRay();
         }
     }
     
@@ -48,5 +39,18 @@ public class PlayerController_Test : MonoBehaviour
     {
         Vector3 ShotPos = this.transform.position + new Vector3(0.5f, 0, 0.3f);
         Instantiate(ballPrefab, ShotPos, Quaternion.identity);
+    }
+    
+    private void ShotRay()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(displayCenter);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            pos = hit.collider.gameObject.transform;   //rayの当たったオブジェクトの座標を取得
+            block = hit.collider.gameObject;
+            MapManager.instance.ChangeBlock(block, pos, DOWN);
+            
+        }
     }
 }
