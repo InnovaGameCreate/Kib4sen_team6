01d018db3d60549e93c3fc3768c0ce1e78dc3d3d
s4r6 <kouta4466@icloud.com>
2023-02-17 16:37:46 +0900

    現状の保存
    とりあえず現在の状況を保存

--- Assets/Script/SnowBall.cs
+++ Assets/Script/SnowBall.cs
@@ -7,18 +7,36 @@ public class SnowBall : MonoBehaviour
     const int UP = 1;   //盛り上がる時のフラグ
     [SerializeField]
     private float LifeTime;
+    private Vector3 prePosition;
+    private Vector3 startPosition;
+    public float Timer;
+    private bool Flag;
     // Start is called before the first frame update
     void Start()
     {
+        startPosition = this.transform.position;
         Destroy(this.gameObject, LifeTime);
+        prePosition = this.transform.position;
+        Timer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Vector3.Distance(this.transform.position, prePosition) >= 0.5)
+        {
+            Flag = true;
+        }
         //Debug.Log(ballRigid.velocity.magnitude);　//速度表示
+        if (Flag)
+        {
+            Timer += Time.deltaTime;
+            if(Timer >= 1f)
+            {
+                Debug.Log(Vector3.Distance(this.transform.position, startPosition));
+            }
+        }
     }
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
