529654ae648d4872b566c81be4ed8280f9529ab9
s4r6 <kouta4466@icloud.com>
2023-02-22 06:07:01 +0900

    敵の移動
    謎に回転し続けるので改善が必要

--- Assets/Script/SnowBall.cs
+++ Assets/Script/SnowBall.cs
@@ -7,28 +7,28 @@ public class SnowBall : MonoBehaviour
     const int UP = 1;   //盛り上がる時のフラグ
     [SerializeField]
     private float LifeTime;
-    private Vector3 prePosition;
-    private Vector3 startPosition;
-    public float Timer;
-    private bool Flag;
+    //private Vector3 prePosition;
+    ///private Vector3 startPosition;
+    //public float Timer;
+    //private bool Flag;
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = this.transform.position;
+        //startPosition = this.transform.position;
         Destroy(this.gameObject, LifeTime);
-        prePosition = this.transform.position;
-        Timer = 0f;
+        //prePosition = this.transform.position;
+        //Timer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(this.transform.position, prePosition) >= 0.5)
+        /*if (Vector3.Distance(this.transform.position, prePosition) >= 0.5)
         {
             Flag = true;
-        }
+        }*/
         //Debug.Log(ballRigid.velocity.magnitude);　//速度表示
-        if (Flag)
+        /*if (Flag)
         {
             Timer += Time.deltaTime;
             if(Timer >= 1f)
@@ -36,12 +36,13 @@ public class SnowBall : MonoBehaviour
                 Debug.Log(Vector3.Distance(this.transform.position, startPosition));
             }
         }
+        */
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Player")
             Debug.Log("hit");
-        Destroy(this.gameObject);
+        //Destroy(this.gameObject);
         /*if (collision.gameObject.tag == "Grand")
         {
             MapManager.instance.ChangeBlock(collision.gameObject, collision.gameObject.transform, UP);  //ブロックの設置
