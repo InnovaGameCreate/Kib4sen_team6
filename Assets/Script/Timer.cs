52b0cb0148f049a2c75af4edd8a8f64d4d7d440c
s4r6 <kouta4466@icloud.com>
2023-02-22 20:23:29 +0900

    とりあえず保存

--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -5,25 +5,19 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] private float counttime;
+    private float counttime;
     private float minute;
     private float second;
     // Start is called before the first frame update
     void Start()
     {
-        
+        counttime = GameManager.instance.LimitTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        counttime -= Time.deltaTime;
-        minute = (int)counttime / 60;
-        second = (int)counttime % 60;
-        if (second > 10)
-            GetComponent<Text>().text = minute.ToString() + "•ª" + second.ToString() + "•b";
-        else
-            GetComponent<Text>().text = minute.ToString() + "•ª " + second.ToString() + "•b";
+        
 
     }
 }
